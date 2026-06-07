17ef245c0c71c5084a620991b8062b7c75028d5a
d3fd44 <mhdmomendefda359@gmail.com>
2026-06-07 18:00:59 +0300

    add `ts_ls` support for jsx.

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -31,7 +31,7 @@ vim.lsp.enable('pyright')
 
 vim.lsp.config['ts_ls'] = {
     cmd = { 'typescript-language-server', '--stdio' },
-    filetypes = { "javascript", "typescript", "typescriptreact" },
+    filetypes = { "javascript", "typescript", "typescriptreact", "javascriptreact" },
     capabilities = capabilities,
 }
 vim.lsp.enable('ts_ls')
